f16bf8be3dea24c62ea985c47b04c90cb38360ea
vista-juan-constantine <juan.constantine@vistadash.com>
2025-05-09 00:00:55 -0500

    Install spatie/laravel-query-builder

--- app/Http/Controllers/api/v1/MechanicalWorkshopController.php
+++ app/Http/Controllers/api/v1/MechanicalWorkshopController.php
@@ -6,12 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\MechanicalWorkshopResource;
 use App\Models\MechanicalWorkshop;
 
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
 class MechanicalWorkshopController extends Controller
 {
 
     public function index()
     {
         $mechanical_workshops = MechanicalWorkshop::all();
+
+        $mechanical_workshops = QueryBuilder::for(MechanicalWorkshop::class)
+            ->allowedFilters([
+                AllowedFilter::exact('ciudad')
+            ])
+            ->get();
+
         $response = MechanicalWorkshopResource::collection($mechanical_workshops);
 
         return $this->successfulMessageShowAll($response);
