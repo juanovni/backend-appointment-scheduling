2f4b842485cfacb1eb4caf85c4dcb813fffbd7be
vista-juan-constantine <juan.constantine@vistadash.com>
2025-05-20 16:17:37 -0500

    Logo temporarily added

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="225px" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
     <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
         <a href="">
-            <img alt="Logo" src="{{ asset('assets/media/logos/grepicker_logo.png') }}" class="h-48px app-sidebar-logo-default" width="140px" style="margin-left: 28px;" />
-            <img alt="Logo" src="{{ asset('assets/media/logos/grelive_logo_01.png') }}" class="h-40px app-sidebar-logo-minimize" />
+            <img alt="Logo" src="{{ asset('assets/media/logos/default-dark.svg') }}" class="h-25px app-sidebar-logo-default" width="140px" style="margin-left: 28px;" />
+            <img alt="Logo" src="{{ asset('assets/media/logos/default-small.svg') }}" class="h-20px app-sidebar-logo-minimize" />
         </a>
         <div id="kt_app_sidebar_toggle" class="app-sidebar-toggle btn btn-icon btn-shadow btn-sm btn-color-muted btn-active-color-primary h-30px w-30px position-absolute top-50 start-100 translate-middle rotate {{{ (isset($_COOKIE["sidebar_minimize_state"]) && $_COOKIE["sidebar_minimize_state"]=='on') ? 'active' : '' }}}" data-kt-toggle="false" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="app-sidebar-minimize">
             <i class="ki-duotone ki-black-left-line fs-3 rotate-180">
